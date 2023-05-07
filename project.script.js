2590a20f87d8fe18ff257e0588a0fdd2e182b5e4
alvimdev <alvim103@gmail.com>
2023-05-07 00:07:06 -0300

    'ending' the project
    corrigi muitos dos bugs que haviam e implementei o .js (obrigado eustaquio e gepeto). Além de estilizar melhor o site e adicionar os tênis (imagens ruins, eu sei, mas fzr oq né?)

---
+++ project.script.js
@@ -0,0 +1,23 @@
+var totas_as_divisoes = ["all", "nike", "puma", "adidas", "asics"];
+var todos_os_botoes = ["botao_all", "botao_nike", "botao_puma", "botao_adidas", "botao_asics"];
+var divisao_atual = undefined;
+
+function hide(element_id){
+    document.getElementById(element_id).style.display = 'none';
+}
+
+function flex(element_id){
+    document.getElementById(element_id).style.display = 'flex';
+}
+
+function mudarDivisaoDaPagina(element_id){
+    if(element_id === 'all'){
+        divisao_atual = element_id;
+        totas_as_divisoes.forEach(flex);
+    }
+    else{
+        divisao_atual = element_id
+        totas_as_divisoes.forEach(hide);
+        document.getElementById(element_id).style.display = 'flex';
+    }
+}
